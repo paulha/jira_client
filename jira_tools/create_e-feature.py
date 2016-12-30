222a5ce3f799e3f3f0f5cf009a4dfeab4d4f23d2
Sean McKeon <sean.wx.mckeon@intel.com>
2016-12-30 11:12:25 -0800

    Efeature creation for CREQ complete.

--- jira_tools/create_e-feature.py
+++ jira_tools/create_e-feature.py
@@ -7,19 +7,8 @@ from jirafields import make_field_lookup
 
 PROJECT = 'CREQ'
 
-areqs= [
-('AREQ-16492','Glenview','N','pzou1','ruili1x',['']),
-('AREQ-11608','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-11607','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-11606','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-10268','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-389','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-327','Glenview','N','eepshtey','maxiaoso',['']),
-('AREQ-265','Glenview','N','eepshtey','maxiaoso',[''])
-]
-
-ASSIGNEE = None
-VAL_LEAD = None
+ASSIGNEE = 'dajakli'
+VAL_LEAD = 'dajakli'
 
 if __name__ == "__main__":
     i = 0
@@ -30,29 +19,31 @@ if __name__ == "__main__":
     val_lead_key = fl.reverse('Validation Lead')
     and_vers_key = fl.reverse('Android Version(s)')
     platprog_key = fl.reverse('Platform/Program')
-    exist_on_key = fl.reverse('Exists On')
 
-    jql = """project = CREQ AND issuetype = Feature AND assignee = swmckeox"""
+    #jql = """project = CREQ AND issuetype = Feature AND assignee = swmckeox"""
+    jql = """project = %s AND issuetype = Feature"""%(PROJECT,)
     for areq in jql_issue_gen(jql, j):
-        print areq.key, areq.fields.summary
+        print areq.key, areq.fields.summary, getattr(areq.fields, and_vers_key)[0].value
 
         for platprog in getattr(areq.fields, platprog_key):
             # Create Sub-task
             i += 1
             issue_dict = {
-                'project': {'key': PROJECT},
+                'project': {'key': areq.fields.project.key},
                 'parent': {'key': areq.key},
                 'summary': 'TBD',
                 'issuetype': {'name': 'E-Feature'},
                 'assignee': {'name': ASSIGNEE},
                 val_lead_key: {'name':VAL_LEAD},
-                #and_vers_key: [{'value':getattr(areq.fields, and_vers_key)[0].value}],
+                and_vers_key: [{'value':getattr(areq.fields, and_vers_key)[0].value}],
+                #and_vers_key: [{'value':'4.4'}],
                 platprog_key: [{'value':platprog.value}]
             }
 
-            print i, areq.key, platprog.value
-            efeature = j.create_issue(fields=issue_dict)
-            new_sum = efeature.fields.summary.replace('[]', '[4.4]')
-            efeature.update(fields={'summary':new_sum})
-            print efeature.key, efeature.fields.summary
+            ### Nerfed for safety
+            #print i, areq.key, platprog.value
+            #efeature = j.create_issue(fields=issue_dict)
+            #new_sum = efeature.fields.summary.replace('[]', '[4.4]')
+            #efeature.update(fields={'summary':new_sum})
+            #print "\t", efeature.key, efeature.fields.summary, getattr(efeature.fields, and_vers_key)[0].value
 
