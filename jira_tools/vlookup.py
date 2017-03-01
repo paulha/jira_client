2f28c6552f95ccb255240939638dbd083b6db8a1
WilcoX.JoshuaX <joshuax.wilcox@intel.com>
2017-03-01 14:53:34 -0800

    AREQ-22296 - added path logic to handle Assignee, Validation Lead, Components, and Priority. no error handling.

--- jira_tools/vlookup.py
+++ jira_tools/vlookup.py
@@ -51,98 +51,105 @@ def update_status(source, destination):
     if (transition_id):
         transition_issue(destination, transition_id)
 
-def vlookup_data(map_file):
+def vlookup_data(jira, map_file):
     for preq in map_file:
         # Print
         print("GID %s matches N-dessert %s and O-dessert %s" % (preq['Global ID'], preq['O-dessert'], preq['N-dessert']))
-        continue
-    return 0
+        source = jira.issue(preq['N-dessert'])
+        destination = jira.issue(preq['O-dessert'])
+        # Updated Fields
+        updated_fields = {}
 
-    print areq[0] + " -> " + areq[1]
-    source = jira.issue(areq[0])
-    destination = jira.issue(areq[1])
+        # Components
+        all_components = []
+        for component in source.fields.components:
+            all_components.append({"name": component.name})
+        if all_components:
+            updated_fields["components"] = all_components
 
-    # Updated Fields
-    updated_fields = {}
+        # Assignee
+        assignee = source.fields.assignee
+        if assignee:
+            updated_fields["assignee"] = {"name": assignee.name}
 
-    # Components
-    all_components = []
-    for component in source.fields.components:
-        all_components.append({"name": component.name})
-    if all_components:
-        updated_fields["components"] = all_components
+        # Validation Lead
+        validation_lead = source.fields.customfield_19700
+        if validation_lead:
+            updated_fields["customfield_19700"] = {"name": validation_lead.name}
+
+        # Priority
+        priority = source.fields.priority
+        if priority:
+            updated_fields["priority"] = {"name": priority.name}
+
+        # Update
+        if updated_fields:
+            destination.update(fields=updated_fields)
+        
+    return len(updated_fields)
+
+#    print areq[0] + " -> " + areq[1]
+#    source = jira.issue(areq[0])
+#    destination = jira.issue(areq[1])
+
 
     # Fix Version/s
-    fix_versions = []
-    for version in source.fields.fixVersions:
-        fix_versions.append({"name": version.name})
-    if fix_versions:
-        updated_fields["fixVersions"] = fix_versions
+#    fix_versions = []
+#    for version in source.fields.fixVersions:
+#        fix_versions.append({"name": version.name})
+#    if fix_versions:
+#        updated_fields["fixVersions"] = fix_versions
 
     # Exists On
-    exists_on = []
-    if source.fields.customfield_18202:
-        for target in source.fields.customfield_18202:
-            exists_on.append({"value": target.value})
-    if exists_on:
-        updated_fields["customfield_18202"] = exists_on
+#    exists_on = []
+#    if source.fields.customfield_18202:
+#        for target in source.fields.customfield_18202:
+#            exists_on.append({"value": target.value})
+#    if exists_on:
+#        updated_fields["customfield_18202"] = exists_on
 
     # Verified On
-    verified_on = []
-    if source.fields.customfield_15301:
-        for target in source.fields.customfield_15301:
-            verified_on.append({"value": target.value})
-    if verified_on:
-        updated_fields["customfield_15301"] = verified_on
+#    verified_on = []
+#    if source.fields.customfield_15301:
+#        for target in source.fields.customfield_15301:
+#            verified_on.append({"value": target.value})
+#    if verified_on:
+#        updated_fields["customfield_15301"] = verified_on
 
     # Planned Release
-    if source.fields.customfield_18400:
-        updated_fields["customfield_18400"] = {"value": source.fields.customfield_18400.value}
+#    if source.fields.customfield_18400:
+#        updated_fields["customfield_18400"] = {"value": source.fields.customfield_18400.value}
 
     # Actual Release
-    if source.fields.customfield_17803:
-        updated_fields["customfield_17803"] = {"value": source.fields.customfield_17803.value}
+#    if source.fields.customfield_17803:
+#        updated_fields["customfield_17803"] = {"value": source.fields.customfield_17803.value}
 
     # Test Date/Time
-    if source.fields.customfield_12121:
-        updated_fields["customfield_12121"] = source.fields.customfield_12121
-
-    # Assignee
-    assignee = source.fields.assignee
-    if assignee:
-        updated_fields["assignee"] = {"name":assignee.name}
-
-    # Validation Lead
-    validation_lead = source.fields.customfield_19700
-    if validation_lead:
-        updated_fields["customfield_19700"] = {"name": validation_lead.name}
+#    if source.fields.customfield_12121:
+#        updated_fields["customfield_12121"] = source.fields.customfield_12121
 
     # Labels
-    if source.fields.labels:
-        destination.update(fields={"labels": source.fields.labels})
-
-    # Update
-    if updated_fields:
-        destination.update(fields=updated_fields)
+#    if source.fields.labels:
+#        destination.update(fields={"labels": source.fields.labels})
 
     # Status
-    #update_status(source, destination)
+#    update_status(source, destination)
 
     # Link
-    #jira.create_issue_link("Duplicate",source,destination)
+#    jira.create_issue_link("Duplicate",source,destination)
 
     # Link and Comment
-    jira.create_issue_link("Duplicate",source,destination,
-        comment={"body": "Platform Requirements have been transitioned to iCDG JAMA instance.\n\r\n\rReplacing '%s' --> '%s'" % (source.key, destination.key)})
+#    jira.create_issue_link("Duplicate",source,destination,
+#        comment={"body": "Platform Requirements have been transitioned to iCDG JAMA instance.\n\r\n\rReplacing '%s' --> '%s'" % (source.key, destination.key)})
 
     # Reject Source
-    jira.transition_issue(source, 51)
+#    jira.transition_issue(source, 51)
 
 
 if __name__ == "__main__":
-#   jira = init_jira()
-#   import .xlsx file where first worksheet
-    preqs = pe.iget_records(file_name="vlookup.xlsx")
-    vlookup_data(preqs)
-    print "Operation complete."
+    jira = init_jira()
+#   import .xlsx file where first worksheet is A1=Global ID, B1=N-dessert, C1=O-dessert
+    preqs = pe.iget_records(file_name="vlookup_test.xlsx")
+    changes_made = vlookup_data(jira, preqs)
+    print "Operation complete. %d changes made."%changes_made
     
