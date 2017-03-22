606e9e34dea434d291e8a9eeb71492ea4e10a47b
WilcoX.JoshuaX <joshuax.wilcox@intel.com>
2017-03-22 01:19:52 -0700

    AREQ-22968 - ran test query through and it did trigger error handling because I left the dedupe logic in. the output file seems to have worked and now works as a sort of psuedo-logfile in case anything happens then I won't have to restart the whole thing; just do the delta the next time.

--- jira_tools/create_e-feature.py
+++ jira_tools/create_e-feature.py
@@ -1,16 +1,11 @@
-
+import argparse
 import csv
-
 from gojira import init_jira, jql_issue_gen, issue_keys_issue_gen
 from jirafields import make_field_lookup
 
-#PROJECT = 'AREQ'
-#ISSUETYPE = 'E-Feature'
-#ASSIGNEE = ''
-#VAL_LEAD = 'wilcoxjx'
-AND_VER = "O"
+#AND_VER = "O"
 #PLATFORM = 'Broxton-P IVI"'
-droid_ver_id = 'customfield_10811'
+#droid_ver_id = 'customfield_10811'
 
 def create_from_jql(j):
     i = 0
@@ -135,144 +130,88 @@ def clone_efeature_add_dessert(j, jql, doing_list, target_platform, target_desse
         # * same platform, new dessert
         # * same dessert, new platform
         # current flow is: start with source child -> get to parent by ID -> get parent metadata and cut new e-feature into new platform
+        try:
+    
+            # capture some info from each issue as it gets iterated over
+            parent_key = issue.fields.parent.key
+            doing_list += [{'parent': parent_key, 'source_child': issue.key, 'summary': issue.fields.summary}]
+
+            # the new e-features should hit this as TRUE if the AREQ requester defined this as an SD;NP: request
+            if target_platform:
+                child_platform = target_platform
+            else: # new e-features should hit this as FALSE if the AREQ requester defined this as an SP;ND: request
+                child_platform = getattr(issue.fields, platprog_key)[0].value
+
+            # set the assignee
+            if unassign_new:
+                target_assign = "Unassigned"
+            else:
+                target_assign = issue.fields.assignee.name
         
-        # capture some info from each issue as it gets iterated over
-        parent_key = issue.fields.parent.key
-        doing_list += [{'parent': parent_key, 'source_child': issue.key, 'summary': issue.fields.summary}]
+            if target_dessert: ### TODO NOT WORKING TODO NOT WORKING TODO ###
+                # if a new dessert letter is passed, navigate the parent subtask-list for each source child's parent
+                find_parent = "key = %s"%parent_key
+                parent_feature = j.search_issues("key=%s"%issue.fields.parent.key, 0)[0]
+                parent_subtasks = list(parent_feature.fields.subtasks)
 
-        # the new e-features should hit this as TRUE if the AREQ requester defined this as an SD;NP: request
-        if parent_platform:
-            child_platform = parent_platform
-        else: # new e-features should hit this as FALSE if the AREQ requester defined this as an SP;ND: request
-            child_platform = getattr(issue.fields, platprog_key)[0].value
-
-        # set the assignee
-        if unassign_new:
-            target_assign = "Unassigned"
-        else:
-            target_assign = issue.fields.assignee.name
-        
-        if target_dessert: ### TODO NOT WORKING TODO NOT WORKING TODO ###
-            # if a new dessert letter is passed, navigate the parent subtask-list for each source child's parent
-            find_parent = "key = %s"%parent_key
-            parent_feature = j.search_issues("key=%s"%issue.fields.parent.key, 0)[0]
-            parent_subtasks = list(parent_feature.fields.subtasks)
-
-            dupe_list = []
-            for subtask in parent_subtasks:
-                find_subtask_dessert = "key = %s"%subtask.key
-                subtask_info = j.search_issues(find_subtask_dessert, 0)[0]
-                subtask_dessert_version = getattr(subtask_info.fields, and_vers_key)[0].value
-                if (subtask_dessert_version == "O"):
-                    this_issue_prio = [{'issue_id': issue.key, 'parent_id': issue.fields.parent.key, 'clone_id': subtask.key, 'pri_name': issue.fields.priority.name, 'pri_id': issue.fields.priority.id}]
-                    dupe_list.append(subtask_info.key)
-                    # create dupe list
-            if dupe_list:
-                doing_list += [{'source_id': issue.key, 'summary': 'dupe;no-clone'}]
-                print "already got %s"%issue.key
-        else: ### TODO ALL SHOULD CURRENTLY GO THIS ROUTE AS OF 21MAR2017 TODO ###
-
-        new_efeature_dict = {
-            'project': {'key': issue.fields.project.key},
-            'parent': {'key': issue.fields.parent.key},
-            'summary': issue.fields.summary,
-            'issuetype': {'name': 'E-Feature'},
-            'assignee': {'name': target_assign},
-            and_vers_key: [{'value': target_dessert}],
-            platprog_key: [{'value': child_platform}]
-        }
-
-        print "creating new clone of %s"%issue.key
-        efeature = jira.create_issue(fields=new_efeature_dict) 
-        print efeature.key
-        update_count += 1
+                dupe_list = []
+                for subtask in parent_subtasks:
+                    find_subtask_dessert = "key = %s"%subtask.key
+                    subtask_info = j.search_issues(find_subtask_dessert, 0)[0]
+                    subtask_dessert_version = getattr(subtask_info.fields, and_vers_key)[0].value
+                    if (subtask_dessert_version == "O"):
+                        this_issue_prio = [{'issue_id': issue.key, 'parent_id': issue.fields.parent.key, 'clone_id': subtask.key, 'pri_name': issue.fields.priority.name, 'pri_id': issue.fields.priority.id}]
+                        dupe_list.append(subtask_info.key)
+                        # create dupe list
+                if dupe_list:
+                    doing_list += [{'source_id': issue.key, 'summary': 'dupe;no-clone'}]
+                    print "already got %s"%issue.key
+            else: ### TODO ALL SHOULD CURRENTLY GO THIS ROUTE AS OF 21MAR2017 TODO ###
+                new_efeature_dict = {
+                    'project': {'key': issue.fields.project.key},
+                    'parent': {'key': issue.fields.parent.key},
+                    'summary': issue.fields.summary,
+                    'issuetype': {'name': 'E-Feature'},
+                    'assignee': {'name': target_assign},
+                    and_vers_key: [{'value': target_dessert}],
+                    platprog_key: [{'value': child_platform}]
+                }
+            
+            print "creating new clone of %s"%issue.key
+            efeature = jira.create_issue(fields=new_efeature_dict) 
+            update_count += 1
+        except BaseException:
+            print('cloning process halted and caught fire on issue %s'%issue.key)
+            return doing_list
+ 
     print "Updated {} issues".format(update_count)
     return doing_list
 
-def mycsv_reader(csv_reader): 
-    while True: 
-        try: 
-            yield next(csv_reader) 
-        except csv.Error: 
-            # error handling what you want.
-            print("error caught inside csv reader.")
-            pass
-        continue 
-    return
-  
-def change_priority_by_id(j, q):
-    update_count = 0
-#    for k,v in d.items():
-#        update_priority_dict = {
-#            'key': k,
-#            'project': 'AREQ',
-#            'priority': v
-#        }
-#        jira.create_issue(fields=update_priority_dict)
-#        print(update_priority_dict)
-    results = j.search_issues(q,0)
-    target_subtasks = []
-    for issue in results:
-        parent_id = issue.fields.parent.key
-        parent_feature = j.search_issues("key=%s"%parent_id, 0)[0]
-        parent_subtask_keys = list(parent_feature.fields.subtasks)
-        for subtask in parent_subtask_keys:
-            find_subtask_dessert = "key = %s"%subtask.key
-            subtask_info = j.search_issues(find_subtask_dessert, 0)[0]
-            subtask_dessert_version = getattr(subtask_info.fields, and_vers_key)[0].value
-            if (subtask_dessert_version == "O"):
-                target_subtasks
-
-
-
-        update_count += 1
-
-    return update_count
-
 if __name__ == "__main__":
     jira = init_jira()
-    test_jql = """key = AREQ-18873"""
+    test_jql = """key = AREQ-21218"""
     done_list = []
+    
+    # get input from cmd line - TODO to be used for direction to either New Platform or New Dessert functions later
+    parser = argparse.ArgumentParser(description='Use this weird trick to save 600% time on cutting your *NEW* Android Requirement E-Features from home and make $7253 a month that insurance companies in OREGON don\'t want YOU to know!!')
+    parser.add_argument('-i','--input', help='Input file name',required=False)
+    args = parser.parse_args()
 
     # SDNP = same dessert; new platform / SPND = same platform; new dessert
-    # set this according to AREQ request
-    SDNP = True
-    SPND = False
-    if SDNP:
+    # set this according to AREQ request using cmd line args above
+    hasNewPlatform = True ## set to True for AREQ-22968
+    hasNewDessert = False ## * * * * *
+    if hasNewPlatform:
         target_platform = "Icelake-U SDC"
-        target_dessert = ""
-    else:
+        target_dessert = "O" # set to O for AREQ-22968
+    if hasNewDessert:
         target_platform = ""
         target_dessert = "O"
 
     amy_jql = """project = AREQ AND issuetype = E-Feature AND status in (Open, "In Progress", Closed, Merged, Blocked) AND "Android Version(s)" in (O) AND "Platform/Program" in ("Broxton-P IVI") ORDER BY key ASC"""
-    completed = clone_efeature_add_dessert(jira, amy_jql, done_list, target_platform, target_dessert, True)
+    completed = clone_efeature_add_dessert(jira, test_jql, done_list, target_platform, target_dessert, True)
 
-#    colnames = ['id', 'priority']
-#    data = pandas.read_csv('formatted_pri_list.csv', names=colnames)
-#    prilist = data.priority.tolist()
-#    idlist = data.id.tolist()
-#    for i in idlist:
-#        prilist[i].append(idlist[i])
-#    print (prilist)
-
-#    with open('formatted_pri_list.csv', 'rU') as infile:
-#        reader = csv.reader(infile)
-#        mydict = dict(reader)
-# open the file in universal line ending mode 
-#    with open('formatted_pri_list.csv', 'rU') as infile:
-        # read the file as a dictionary for each row ({header : value})
-#        reader = csv.DictReader(infile)
-#        csv_data = {}
-#        for row in reader:
-#            for header, value in row.items():
-#                try:
-#                    csv_data[header].append(value)
-#                except KeyError:
-#                    csv_data[header] = [value]
-#    with open('formatted_pri_list.csv', 'rU') as f:
-#        reader = csv.reader(f, dialect=csv.excel_tab)
-#        csv_in_list = list(reader)
-#    completed = change_priority_by_id(jira, test_jql)
-    print "updated %d rows"%completed
+    thefile = open('test.txt', 'w')
+    for item in completed:
+        thefile.write("%s\n" % item)
 
